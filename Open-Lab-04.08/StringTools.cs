5b8a33689fbde6a42e1b486695007b29a82a72ee
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-12 00:17:35 +0100

    getting a hang of this

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -6,7 +6,25 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    a = a + 1;  
+                }
+            }
+            string[] vysledokstrings = new string[a];
+            for (int ii = 0; ii < strings.Length; ii++)
+            {
+                if (strings[ii].Length == 4)
+                {
+                    vysledokstrings[b] = strings[ii];
+                    b = b + 1;
+                }
+            }
+            return vysledokstrings;
         }
     }
 }
